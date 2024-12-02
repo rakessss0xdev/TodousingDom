ca9f52cc8c0ac9d83ae40906921daa819e528f66
rakeshcodes123 <178692736+rakeshcodes123@users.noreply.github.com>
2024-12-02 07:11:36 +0530

    Finished setting up Dom

--- src/script.js
+++ src/script.js
@@ -1,16 +1,25 @@
 const inputbox = document.getElementById('input-box');
 const listcontainer = document.getElementById('list-container');
 
-function addTodo () {
-    if(inputbox.value === ""){
+function addTodo() {
+    if (inputbox.value === "") {
         alert('Please Enter Your Todo');
-    }
-    else {
+    } else {
         let li = document.createElement("li");
         li.textContent = inputbox.value;
+
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7'; // Unicode for '×'
+        span.className = 'close';
+
+        // Add event listener for the close button
+        span.addEventListener('click', () => {
+            li.remove();
+        });
+
+        li.appendChild(span);
         listcontainer.appendChild(li);
-        let span = document.createElement('span')
-        span
     }
 
+    inputbox.value = '';
 }
